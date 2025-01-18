e686aa1193a23455f3e31882e9dcbb25abd5f48e
Zahra-shadmani <zahradarya09@gmail.com>
2025-01-18 22:42:23 +0330

    first commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,17 @@
 import { title } from "@/components/primitives";
-
+import { Image } from "@heroui/image";
 export default function AboutPage() {
   return (
     <div>
+      <div className="w-full">
+        <Image
+          isZoomed
+          alt="Image with Zoom"
+          src="/1.jpg"
+          width={950}
+          height={500}
+        />
+      </div>
       <h1 className={title()}>About</h1>
     </div>
   );
